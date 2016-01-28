rotation is broken. Im fixing it now.

lib/atom.py
@@ -226,9 +226,11 @@ class Atom:
     def rotation(self, axis, center, theta=None):
         'Rodrigues\'s formula'
         assert len(axis) == 3, 'vector must be 3dimension'
+        assert len(center) == 3, 'vector must be 3dimension'
+        print('rotation in atom')
         vec = np.array(axis)
         cen = np.array(center)
-        vec = vec - cen
+        # vec = vec - cen
         if theta is None:
             theta = np.linalg.norm(vec)
         else:
@@ -242,6 +244,7 @@ class Atom:
             (vz, 0, -vx),
             (-vy, vx, 0)))
         M = I + R * np.sin(theta) / theta + R.dot(R) * (1-np.cos(theta)) / (theta**2)
+        self.coordinate -= cen
         self.transform(M)
         self.coordinate += cen
 

lib/info.py
@@ -227,7 +227,7 @@ class Info:
 
     @chgsum.setter
     def chgsum(self, chgsum):
-        self._chgsum = np.array(chgsum)
+        self._chgsum = np.array(list(map(np.array, chgsum)))
 
     @property
     def chgdif(self):
@@ -235,7 +235,7 @@ class Info:
 
     @chgdif.setter
     def chgdif(self, chgdif):
-        self._chgdif = np.array(chgdif)
+        self._chgdif = np.array(list(map(np.array, chgdif)))
 
     @property
     def chgcell(self):